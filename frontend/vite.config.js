4acd6814c7e40f9966b58a424c4dbce95f4159ed
Duncan Ombiro <duncanmakoyo@gmail.com>
2025-11-30 20:21:11 +0300

    Set Vite base for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // <-- this fixes the white screen on Render
+  base: './',   // must be set for correct asset paths
 })
